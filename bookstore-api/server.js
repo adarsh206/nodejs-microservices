d4ccbe3ffe882231384fc1dd7607b93a4f49a902
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-01-10 14:50:54 +0530

    bookstore-api started

---
+++ bookstore-api/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const connectToDB = require("./database/db");
+const bookRoutes = require("./routes/book-routes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//connect to our database
+connectToDB();
+
+//middleware -> express.json()
+app.use(express.json());
+
+// to allow the frontend to access backend services
+app.use(cors())
+
+//routes here
+app.use("/api/books", bookRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is now running on port ${PORT}`);
+});
