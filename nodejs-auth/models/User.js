bfb3b20c06d90b513420706f3f2be8000aa38351
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-01-10 18:16:16 +0530

    user services added

--- nodejs-auth/models/User.js
+++ nodejs-auth/models/User.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    user : {
+    username : {
         type : String,
         required : true,
         unique : true,
--- nodejs-auth/server.js
+++ nodejs-auth/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectToDB = require('./database/db');
+const authRoutes = require("./routes/auth-routes");
 
 
 // database connection
@@ -9,6 +10,7 @@ connectToDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is now listening at ${PORT}`);
