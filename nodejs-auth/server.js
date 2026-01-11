c19570d2a2ca7e09a04b500d49c2605a5c2c6abb
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-01-11 11:51:12 +0530

    authentication added

--- nodejs-auth/server.js
+++ nodejs-auth/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const connectToDB = require('./database/db');
 const authRoutes = require("./routes/auth-routes");
+const homeRoutes = require("./routes/home-routes");
+const adminRoutes = require("./routes/admin-routes");
 
 
 // database connection
@@ -11,6 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/home", homeRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is now listening at ${PORT}`);
