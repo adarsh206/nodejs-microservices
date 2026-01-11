360d52ab9c42bf135e8e4fd2c6d9f6079c454cf2
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-01-11 14:32:26 +0530

    image upload functionality added

--- nodejs-auth/server.js
+++ nodejs-auth/server.js
@@ -4,6 +4,7 @@ const connectToDB = require('./database/db');
 const authRoutes = require("./routes/auth-routes");
 const homeRoutes = require("./routes/home-routes");
 const adminRoutes = require("./routes/admin-routes");
+const uploadImageRoutes = require("./routes/image-routes")
 
 
 // database connection
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/auth", authRoutes);
 app.use("/api/home", homeRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/image", uploadImageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is now listening at ${PORT}`);
