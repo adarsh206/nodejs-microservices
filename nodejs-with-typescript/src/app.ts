05a829313dfa5194f6e51a36fd177b7b05db6045
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-01-25 12:34:48 +0530

    mongoose with typescript added

--- nodejs-with-typescript/src/app.ts
+++ nodejs-with-typescript/src/app.ts
@@ -1,4 +1,5 @@
-import express, {Express, Request, Response} from "express";
+import express, {Express, Request, Response, NextFunction} from "express";
+import { IUser, User } from "./models/User";
 
 
 const app : Express = express();
@@ -8,8 +9,15 @@ const PORT = 3000;
 
 app.use(express.json());
 
+interface CustomRequest extends Request {
+    startTime? : number
+}
+
 //middleware -> add startTime to request
-app.use()
+app.use((req: CustomRequest, res : Response, next : NextFunction) => {
+    req.startTime = Date.now();
+    next();
+})
 
 
 // req -> Request<p, ResBody, ReqBody, ReqQuery, Locals>
@@ -19,7 +27,41 @@ app.get("/", (req : Request, res : Response) => {
     res.send("Hello World from Typescript with express")
 })
 
+app.get("/users", async(req : Request, res : Response) => {
+    try {
+        
+        const users : IUser[] = await User.find();
+        res.status(200).json({message : "User fetched successfully"});
 
+    } catch (error) {
+        res.status(400).json({message : "Some Error Occured!"})
+    }
+})
+
+
+// post route -> new user -> name, email -> req.body
+// -> /user/:id?name -> Request <{}, {}, {}, {}, {}>
+
+interface User {
+    name : string,
+    email : string
+}
+
+app.post("/user", (req: Request<{}, {}, User>, res: Response) => {
+    const {name, email} = req.body;
+    res.json({
+        message : `User created ${name} with email ${email}`,
+    })
+})
+
+// users based on id
+app.get('/users/:id', (req: Request<{id: string}>, res: Response) => {
+    const {id} = req.params;
+    res.json({
+        useId : id,
+        message : "User fetched successfully"
+    })
+})
 app.listen(PORT, () => {
     console.log(`Server is now running at port ${PORT}`);
 })
