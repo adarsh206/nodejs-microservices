4eb12c41be6b89b8c437c784cdcc046fa58e7fd7
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-01-26 08:30:04 +0530

    redis completed

---
+++ redis/io-redis.js
@@ -0,0 +1,23 @@
+
+const Redis = require("ioredis");
+// redis client library for nodejs
+
+const redis = new Redis();
+
+async function ioRedisDemo(){
+    try {
+        
+        await redis.set("key", "value");
+        const val = await redis.get("key");
+        console.log(val);
+        
+    } catch (error) {
+        console.error(error);
+    }
+    finally
+    {
+        redis.quit();
+    }
+}
+
+ioRedisDemo();
--- redis/pub-sub.js
+++ redis/pub-sub.js
@@ -31,6 +31,69 @@ async function testAdditionalFeatures() {
 
         await subscriber.unsubscribe("dummy-channel");
         await subscriber.quit(); // close the subscriber connection
+
+        // pipelining & transactions
+        const multi = client.multi();
+
+        multi.set("key-transaction1", "value1");
+        multi.set("key-transaction2", "value2");
+        multi.get("key-transaction1");
+        multi.get("key-transaction2");
+
+        const results = await multi.exec();
+        console.log(results);
+
+        const pipeline = client.multi();
+        multi.set("key-pipeline1", "value1");
+        multi.set("key-pipeline2", "value2");
+        multi.get("key-pipeline1");
+        multi.get("key-pipeline2");
+
+        const pipelineresults = await multi.exec();
+        console.log(pipelineresults);
+
+
+        // batch data operations
+        const pipelineOne = client.multi();
+
+        for(let i =0 ;i<1000; i++){
+            pipeline.set(`user:${i}:action`, `Action ${i}`)
+        }
+
+        await pipelineOne.exec()
+
+        const dummyExample = client.multi()
+        multi.decrBy('account:1234:balance', 100)
+        multi.incrBy('account:0000:balance', 100)
+
+        const finalresults = await multi.exec()
+
+        const cartExample = client.multi()
+        multi.hIncrBy('cart:1234', 'item_count', 1)
+        multi.hIncrBy('cart:1234', 'total_price', 10)
+
+        await multi.exec();
+
+        console.log("Performance test");
+        console.time("without pipelining");
+
+        for (let i = 0; i < 1000; i++) {
+             await client.set(`user${i}`, `user_value${i}`);
+        }
+
+        console.timeEnd("without pipelining");
+
+        console.time("with pipelining");
+        const bigPipeline = client.multi();
+
+        for (let i = 0; i < 1000; i++) {
+            bigPipeline.set(`user_pipeline_key${i}`, `user_pipeline_value${i}`);
+        }
+
+        await bigPipeline.exec();
+
+        console.timeEnd("with pipelining");
+
     } catch (error) {
         console.error(error);
     }
