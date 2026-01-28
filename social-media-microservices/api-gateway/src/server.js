8514713a122c67c529f066a47215ee58ae2d1e40
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-01-28 08:20:52 +0530

    proxy added

--- social-media-microservices/api-gateway/src/server.js
+++ social-media-microservices/api-gateway/src/server.js
@@ -6,7 +6,8 @@ const helmet = require("helmet");
 const { rateLimit } = require("express-rate-limit");
 const { RedisStore } = require("rate-limit-redis");
 const logger = require("./utils/logger");
-
+const proxy = require("express-http-proxy");
+const errorHandler = require("./middleware/errorHandler")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 
 //rate limiting
-const ratelimit = rateLimit({
+const ratelimitOptions = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   standardHeaders: true,
@@ -34,7 +35,7 @@ const ratelimit = rateLimit({
 });
 
 
-app.use(ratelimit);
+app.use(ratelimitOptions);
 
 app.use((req, res, next) => {
   logger.info(`Received ${req.method} request to ${req.url}`);
@@ -45,3 +46,38 @@ app.use((req, res, next) => {
 // api-gateway -> /v1/auth/register  -> 3000 -> redirect from here to identity services via proxy
 // identity-services -> api/auth/register -> 3001
 // final api -> localhost:3000/v1/auth/register -> localhost:3001/api/auth/register
+
+
+const proxyOptions = {
+  proxyReqPathResolver: (req) => {
+    return req.originalUrl.replace(/^\/v1/, "/api");
+  },
+  proxyErrorHandler: (err, res, next) => {
+    logger.error(`Proxy error : ${err.message}`);
+    res.status(500).json({
+      message : "Internal server error",
+      error : err.message
+    });
+  },
+};
+
+// setting up proxy for our identity service
+app.use("/v1/auth", proxy(process.env.IDENTITY_SERVICE_URL, {
+  ...proxyOptions,
+  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+    proxyReqOpts.headers["Content-Type"] = "application/json";
+    return proxyReqOpts
+  },
+  userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+    logger.info(`Response received from Identity service : ${proxyRes.statusCode}`);
+    return proxyResData;
+  }
+}));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`API Gateway is running on port ${PORT}`);
+  logger.info(`Identity Service is running on port ${process.env.IDENTITY_SERVICE_URL}`);
+  logger.info(`Redis URL is running on port ${process.env.REDIS_URL}`);
+})
