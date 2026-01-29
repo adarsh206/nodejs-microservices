4c65d31e1896b168585db6b670fbbb6e2401c795
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-01-29 08:21:46 +0530

    refresh token added

--- social-media-microservices/identity-service/src/controllers/identity-controller.js
+++ social-media-microservices/identity-service/src/controllers/identity-controller.js
@@ -1,4 +1,4 @@
-
+const RefreshToken = require("../models/RefreshToken");
 const User = require("../models/User");
 const logger = require("../utils/logger");
 const {validateRegistration} = require("../utils/validation")
@@ -106,9 +106,69 @@ const loginUser = async (req, res) => {
 
 
 // refresh token
+const refreshTokenUser = async (req, res) => {
+  logger.info("Refresh token endpoint hit...");
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      logger.warn("Refresh token missing");
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token missing",
+      });
+    }
+
+    const storedToken = await RefreshToken.findOne({ token: refreshToken });
+    
+    if (!storedToken) {
+      logger.warn("Invalid refresh token provided");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid refresh token",
+      });
+    }
+
+    if (!storedToken || storedToken.expiresAt < new Date()) {
+      logger.warn("Invalid or expired refresh token");
+
+      return res.status(401).json({
+        success: false,
+        message: `Invalid or expired refresh token`,
+      });
+    }
+
+    const user = await User.findById(storedToken.user);
+
+    if (!user) {
+      logger.warn("User not found");
+
+      return res.status(401).json({
+        success: false,
+        message: `User not found`,
+      });
+    }
+
+    const { accessToken: newAccessToken, refreshToken: newRefreshToken } = await generateTokens(user);
+
+    //delete the old refresh token
+    await RefreshToken.deleteOne({ _id: storedToken._id });
+
+    res.json({
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  } catch (e) {
+    logger.error("Refresh token error occured", e);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
 
 // logout
 
 
 
-module.exports = { registerUser, loginUser };
+module.exports = { registerUser, loginUser, refreshTokenUser };
