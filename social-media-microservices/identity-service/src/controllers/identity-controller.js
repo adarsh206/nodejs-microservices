a33d3404afb6615f907ef6d924f3be4212877f55
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-01-30 07:50:30 +0530

    logout functionality added

--- social-media-microservices/identity-service/src/controllers/identity-controller.js
+++ social-media-microservices/identity-service/src/controllers/identity-controller.js
@@ -168,7 +168,45 @@ const refreshTokenUser = async (req, res) => {
 
 
 // logout
+const logoutUser = async (req, res) => {
+  logger.info("Logout endpoint hit...");
+  try {
+    
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) {
+      logger.warn("Refresh token missing");
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token missing",
+      });
+    }
+
+   const storedToken = await RefreshToken.findOneAndDelete({token : refreshToken});
+
+   if (!storedToken) {
+      logger.warn("Invalid refresh token provided");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid refresh token",
+      });
+    }
+
+   logger.info("Refresh token deleted for logout");
+
+   res.json({
+    success : true,
+    message : "Logged out successfully"
+   })
+
+  } catch (error) {
+    logger.error("Error while logging out", error);
+    res.status(500).json({
+      success : false,
+      message : "Internal Server Error"
+    })
+  }
+}
 
 
-
-module.exports = { registerUser, loginUser, refreshTokenUser };
+module.exports = { registerUser, loginUser, refreshTokenUser, logoutUser };
