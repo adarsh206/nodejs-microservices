d34cdf16edbaa5238e223e6e378abce7048f7b34
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-02-01 16:15:50 +0530

    routes and controllers added

---
+++ social-media-microservices/media-service/src/controllers/media-controller.js
@@ -0,0 +1,53 @@
+const Media = require("../models/Media");
+const { uploadMediaToCloudinary } = require('../utils/cloudinary');
+const logger = require('../utils/logger');
+
+const uploadMedia = async (req, res) => {
+    logger.info("Starting media upload");
+    try {
+        
+        if(!req.file){
+            logger.error("No file found. Please add a file and try again!");
+            return res.status(400).json({
+                success : false,
+                message : "No file found. Please add a file and try again!"
+            })
+        }
+
+        const { originalName, mimeType, buffer } = req.file;
+        const userId = req.user.userId;
+
+        logger.info(`File details : ${originalName}, type = ${mimeType}`);
+        logger.info("uploading to cloudinary starting...");
+
+        const cloudinaryUploadResult = await uploadMediaToCloudinary(req.file);
+        logger.info(`Cloudinary upload successfully. Public Id: - ${cloudinaryUploadResult.public_id}`);
+
+        const newlyCreatedMedia = new Media({
+            publicId : cloudinaryUploadResult.public_id,
+            originalName,
+            mimeType,
+            url : cloudinaryUploadResult.secure_url,
+            userId
+        });
+
+        await newlyCreatedMedia.save();
+
+        res.status(200).json({
+            success : true,
+            mediaId: newlyCreatedMedia._id,
+            url: newlyCreatedMedia.url,
+            message: "Media upload is successfully",
+        })
+
+    } catch (error) {
+        logger.info("Error creating media :", error);
+        res.json({
+            success : false,
+            message : "Error creating post"
+        })
+    }
+};
+
+
+module.exports = { uploadMedia };
